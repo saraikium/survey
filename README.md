8fdf1d102274800d7b2de261d1900348fd6c18af
kwanso-adnan <adnan.khan@kwanso.com>
2019-11-28 14:35:48 +0500

    Enhanced backend

---
+++ README.md
@@ -0,0 +1,12 @@
+## Build Setup
+
+``` bash
+
+# install dependencies
+$ npm install -g sequelize-cli mysql2
+$ npm install
+$ sequelize db:create
+$ sequelize db:migrate
+# serve with hot reload at localhost:3000
+$ npm start
+`
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 //@ts-check
 const express = require("express");
 const { json, urlencoded } = require("body-parser");
+const cors = require("cors");
 const router = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
